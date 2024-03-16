94d1ea1bbbddba79ca9176586f65610642e87adf
Abdul <abdultrivial@gmail.com>
2024-03-16 15:14:12 +0530

    validated schemas

--- src/schemas/order.schema.ts
+++ src/schemas/order.schema.ts
@@ -3,44 +3,81 @@ import { Document, Types } from 'mongoose';
 
 export type OrderDocument = Order & Document;
 
-interface ShippingInfo {
+class ShippingInfo {
+  @Prop({ required: true })
   address: string;
+
+  @Prop({ required: true })
   city: string;
+
+  @Prop({ required: true })
   state: string;
+
+  @Prop({ required: true })
   country: string;
+
+  @Prop({ required: true })
   pinCode: string;
+
+  @Prop({ required: true })
   phNumber: string;
 }
 
-interface ItemInfo {
+class ItemInfo {
+  @Prop({ required: true })
   name: string;
+
+  @Prop({ required: true })
   price: string;
+
+  @Prop({ required: true })
   quantity: string;
+
+  @Prop({ required: true, type: Types.ObjectId, ref: 'Product' })
   product: Types.ObjectId;
 }
 
-interface PaymentInfo {
+class PaymentInfo {
+  @Prop({ required: true })
   paymentId: string;
+
+  @Prop({ required: true })
   status: 'Pending' | 'Successful';
+
+  @Prop({ required: true })
   paymentMethod: string;
+
+  @Prop({ required: true })
   price: number;
+
+  @Prop({ required: true })
   itemPrice: number;
+
+  @Prop({ required: true })
   taxPrice: number;
+
+  @Prop({ required: true })
   shippingPrice: number;
+
+  @Prop({ required: true })
   totalPrice: number;
+
+  @Prop({ required: true })
   deliveryDate: Date;
+
+  @Prop({ required: true })
   deliveryStatus: 'Pending' | 'Delivered' | 'Shipped';
 }
 
 @Schema()
 export class Order {
-  @Prop({ required: true })
+  @Prop({ type: ShippingInfo, required: true })
   shippingInfo: ShippingInfo;
 
-  @Prop({ required: true })
+  @Prop({ type: [ItemInfo], required: true })
   itemInfo: ItemInfo[];
 
-  @Prop({ required: true })
+  @Prop({ type: PaymentInfo, required: true })
   paymentInfo: PaymentInfo;
 }
 
